2b77592ca5975f46663f0b0cbf3bab25466d7d71
Aaron Addleman <aaron@9thport.net>
2012-01-21 22:23:43 -0800

    added rails 3 framework files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foodscore::Application.config.secret_token = '2c33bce7d0028c020749e0ddcc9bce891143fe77c65a59e597ad7a09d19e0a3c53c012afb8551d4693f0dbadfe50186c4eac70c814b099dde78f6fcd0f91d838'
